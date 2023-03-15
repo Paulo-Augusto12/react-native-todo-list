bcbab7d13713a8f5276697f71c6ed1b16a3c800e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-15 10:40:37 -0300

    chore(v2): starting navigate development

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
 import { Home } from "./screens/Home";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 export default function App() {
-  return <Home />;
+  const Navigator = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Navigator.Navigator screenOptions={{ headerShown: false }}>
+        <Navigator.Screen name="Home" component={Home} />
+      </Navigator.Navigator>
+    </NavigationContainer>
+  );
 }
