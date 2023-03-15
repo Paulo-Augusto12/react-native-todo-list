3d95020d3f71be9f393f1d6a473718b648c6a157
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-15 10:44:09 -0300

    chore(v2): separating files from component

--- components/Task/index.tsx
+++ components/Task/index.tsx
@@ -2,6 +2,7 @@ import { Check } from "phosphor-react-native";
 import React from "react";
 
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { styles } from "./style";
 
 interface ITaskProps {
   description: string;
@@ -15,12 +16,11 @@ export function Task({
   handleChangeStatus,
   handleNavigate,
 }: ITaskProps) {
-
-  const styles = StyleSheet.create({
+  const propstyles = StyleSheet.create({
     container: {
       padding: 20,
       backgroundColor: "#fff",
-      opacity: completed ? 0.50 : 100,
+      opacity: completed ? 0.5 : 100,
       borderRadius: 10,
       display: "flex",
       flexDirection: "row",
@@ -28,58 +28,27 @@ export function Task({
       flexWrap: "wrap",
       justifyContent: "space-between",
     },
-    initialElements: {
-      display: "flex",
-      flexDirection: "row",
-      gap: 15,
-      alignItems: "center",
-    },
-    square: {
-      width: 28,
-      height: 28,
-      backgroundColor: "#55BCF666",
-      borderRadius: 5,
-    },
     taskDescription: {
       fontSize: 14,
       fontWeight: "400",
       color: "#1A1A1A",
-      textDecorationLine: completed ? 'line-through': 'none',
-    },
-    dot: {
-      width: 12,
-      height: 12,
-      borderWidth: 2,
-      borderColor: "#55BCF666",
-      borderRadius: 5,
-    },
-    completedTask: {
-      width: 28,
-      height: 28,
-      backgroundColor: "#55BCF666",
-      borderRadius: 5,
-      alignItems: "center",
-      justifyContent: "center",
+      textDecorationLine: completed ? "line-through" : "none",
     },
   });
   return (
-    <View style={styles.container}>
+    <View style={propstyles.container}>
       <View style={styles.initialElements}>
         <TouchableOpacity onPress={() => handleChangeStatus()}>
           {completed ? (
             <View style={styles.completedTask}>
-              <Check size={12} color='#1A1A1A'/>
+              <Check size={12} color="#1A1A1A" />
             </View>
           ) : (
             <View style={styles.square} />
           )}
         </TouchableOpacity>
         <TouchableOpacity onPress={() => handleNavigate()}>
-          <Text
-            style={styles.taskDescription}
-          >
-            {description}
-          </Text>
+          <Text style={propstyles.taskDescription}>{description}</Text>
         </TouchableOpacity>
       </View>
       <View>
